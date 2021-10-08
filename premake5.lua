66c3f2c27aff1543c28d6cad6b2cf86cd7bf0048
Frank Xu <jxu129@gmail.com>
2021-10-09 04:27:09 +0800

    -Updated premake5.lua to include the right files

--- premake5.lua
+++ premake5.lua
@@ -7,16 +7,8 @@ project "ImGui"
 
 	files
 	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp",
+        "*.h",
+        "*.cpp",
         "backends/imgui_impl_dx*",
         "backends/imgui_impl_opengl*",
         "backends/imgui_impl_win32*",
